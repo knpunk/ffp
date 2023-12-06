79887021fcddee24f5c8a2d35308ce72dbc056e1
knpunk <101012377+knpunk@users.noreply.github.com>
2023-12-06 12:19:55 +0530

    Add files via upload

---
+++ test_my_module.py
@@ -0,0 +1,17 @@
+# File: test_my_module.py
+
+import unittest
+from my_module import add, subtract
+
+class TestMyModule(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(1, 1), 0)
+
+if __name__ == '__main__':
+    unittest.main()
